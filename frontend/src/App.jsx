3dfa3aa6c23625f32d58333a5ed745efce842088
Malindi <malindidiss123@gmail.com>
2024-09-04 21:40:36 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import PaymentForm from './components/PaymentForm'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminPaymentView from './components/AdminPaymentView';
+import Header from './components/header';
+import Footer from './components/footer';
 
 
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <Router>
+      <div className="App flex flex-col min-h-screen" >
+        <Header/>
+        <div className="flex-grow">
+
+        <Routes>
+
+        <Route path="/payment" element={<PaymentForm />} />
+        <Route path="/admin/payments" element={<AdminPaymentView />} />
+        </Routes>
+        </div>
+        <Footer/>
+      </div>
+    </Router>
   )
 }
 
